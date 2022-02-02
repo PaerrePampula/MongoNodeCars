5cf21bd43776d80b178b0c36743feb415d8d9990
Patrik Kumpula <patrik19100@student.hamk.fi>
2022-02-02 17:26:21 +0200

    updated index.js with routing

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const express = require('express');
 //Enables the usage of json parsing middleware, allows us to read json from any requests with json stuff within the body.
 const bodyParser = require('body-parser');
+//Enables usage of our routing from our custom routing module.
+const routes = require("./routes")
 
 const app = express();
 app.use(bodyParser.json());
-
+//Use the router from the routes.js module.
+app.use('/', routes);
 //Change to port you wish to use, but this port should be fine too.
 const port = 3000;
 
