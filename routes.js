acbcd7ea91ae637d214ee9ec8afe8d36c2ded595
Patrik Kumpula <patrik19100@student.hamk.fi>
2022-02-02 18:06:46 +0200

    rest of the crud operations added to the routing

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const Car = require("./models/car");
 
 //We add the CRUD operations to our initialized router from now on.
 // Fetch all cars
-router.get("/Cars", async (req, res) => 
+router.get("/cars", async (req, res) => 
 {
     try 
     {
@@ -37,5 +37,30 @@ router.post("/cars", async (req, res) =>
 	}
 
 });
+router.delete("/cars", async (req, res) =>
+{
+	await Car.deleteOne({_id : req.body.id }, (err, result) =>
+	{
+		catchErrors(err, result);
+	});
+});
+router.put("/cars", async (req, res) =>
+{
+	await Car.findOneAndUpdate({ _id : req.params.id}, req.body, { new:true}, (err, result) =>
+	{
+		catchErrors(err, result);
+	});
+});
+const catchErrors = (err, result) =>
+{
+	if (err)
+	{
+		return res.status(500).json({ message: err.message });
+	}
+	else
+	{
+		res.status(200).json(result);
+	}
+}
 //Exports for other modules to use the routing.
 module.exports = router;
